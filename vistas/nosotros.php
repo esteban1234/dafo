7d9a93ac18e544fb24b44e0d5d99d2b95599225a
esteban1234 <tva_libra@hotmail.com>
2018-02-13 18:31:03 -0600

    mas cambios

--- vistas/nosotros.php
+++ vistas/nosotros.php
@@ -23,6 +23,52 @@
         <p class="dos">Ofrecemos asesoría personalizada y de alta calidad en nuestras oficinas.</p>
         <a class="boton" href="contacto.php"><button type="button" class="btn btn-danger btn-lg">CONTACTANOS</button></a>
    </div>
+</div> <br><br>
+
+<h1 class="text-center">MISION, VISION, VALORES Y OBJETIVO</h1>
+<hr>
+
+
+<div id="slider">
+<ul id="ul">
+  <li>
+  <div class="slider-container">
+    <h4>MISION</h4>
+    <p>Brindar a nuestros clientes servicios profesionales que superen sus expectativas por medio de nuestro conocimiento, transparencia y ética en las diferentes disciplinas mediante asesoría personalizada y de alta calidad.</p>
+  </div>
+  </li>
+    <li>
+    <div class="slider-container">
+       <h4>VISION</h4>
+    <p>Ser una empresa líder con solidez y reconocimiento en la prestación de servicios legales de excelencia, contando con capacitación continua para nuestros abogados quienes con integridad y profesionalismo resuelvan eficientemente cualquier asunto que concierna a nuestros clientes.</p>
+  </div>
+  </li>
+    <li>
+    <div class="slider-container">
+       <h4>VALORES</h4>
+    <p class="pull-left">
+        <i class="fa fa-check-square"></i> Ética<br>
+        <i class="fa fa-check-square"></i> Honestidad<br>
+        <i class="fa fa-check-square"></i> Lealtad<br>
+        <i class="fa fa-check-square"></i> Respeto <br>
+        <i class="fa fa-check-square"></i> Responsabilidad<br>
+        <i class="fa fa-check-square"></i> Honradez
+    </p>
+    <div class="text-center pull-right" style=" width:200px; height:200px; margin-right: 5%;">
+      <img src="../img/valores.png" alt="" class="img-responsive" width="200px" height="200px">
+    </div>
+  </div>
+  </li>
+    <li>
+    <div class="slider-container">
+      <h4>OBJETIVO</h4>
+    <p> Brindar confianza a nuestros clientes trasmitiendo tranquilidad y seguridad generando relaciones duraderas.</p>
+    <div class="text-center" style="text-align: center; width:150px; height:130px; margin: 0 auto;">
+      <img src="../img/objetivo.png" alt="" class="img-responsive" width="150px" height="155px">
+    </div>
+  </div>
+  </li>
+</ul>
 </div>
 
 
--- vistas/servicios.php
+++ vistas/servicios.php
@@ -1,80 +1,97 @@
 <!DOCTYPE html>
 <?php include 'encabezado2.php' ?>
 <body>
-	<div class="bannermenu2">
-	<div class="contenlogomenu">
-		<figure class="logo">
-			<img src="../img/calcu.png" alt="" class="img-responsive pull-left">
-		</figure>
-		<div id="header__icon" class="menu-opcion">
-		  <i id="open" class="fa fa-align-justify fa-2x" aria-hidden="true"></i>
-		  <i id="close" class="fa fa-close fa-2x close"  aria-hidden="true"></i>
-		  <p>Menu</p>
-		</div>
-		<nav class=" menu pull-right">
-			<ul>
-				<li><a href="../index.php">INDEX</a></li>
-				<li><a href="nosotros.php">NOSOTROS</a></li>
-				<li><a class="select" href="servicios.php">SERVICIOS</a></li>
-				<li><a href="contacto.php">CONTACTO</a></li>
-			</ul>
-		</nav>
-	</div>
+	
+	<header>
+    <figure class="logo pull-left">
+        <img src="../img/dafo.png" alt="DAFO" class="img-responsive">
+    </figure>
+
+    <nav class="pull-right">
+        <ul>
+            <a class="select" href="../index.php">INICIO</a>
+            <a class="select" href="nosotros.php">NOSOTROS</a>
+            <a class="select" id="selector" href="servicios.php">SERVICIOS</a>
+            <a class="select" href="contacto.php">CONTACTO</a>
+        </ul>
+    </nav>
+</header>
+
+<div class="banner3">
+   <div class="fondo3">
+        <p class="uno">DESPACHO DE<br>  ABOGADOS A SU SERVICIO</p>
+        <!-- <p class="dos">Ofrecemos asesoría personalizada y de alta calidad en nuestras oficinas.</p> -->
+        <a class="boton" href="contacto.php"><button type="button" class="btn btn-danger btn-lg">CONTACTANOS</button></a>
+   </div>
 </div>
 
-<article class="empresa">
-	<p>NUESTROS SERVICIOS</p>
-</article>
+<div class="section" style="padding: 5% 0%;">
+      <div class="container">
+      	<h1>ÁREAS DE PRACTRICA</h1><br>
+      	<p>Nuestra empresa ofrece servicios de Litigio, Consultoría legal y Correduría Pública en las siguientes áreas del derecho:</p> <br><br>
+        <div class="row">
+          <div class="col-md-3">
+            <figure class="imagens">
 
-<div class="somos">
-	<p class="pull-right">Nuestro equipo de expertos le prestará un servicio y atención personalizada en las instalaciones de su empresa, implementando soluciones innovadoras que garanticen resultados.</p>
-	<article class="boton2"><a href="contacto.php">Contáctanos</a></article>
-</div>
+            </figure>
+            <h3 class="text-center">DERECHO ADMINISTRATIVO</h3>
+            <p class="">Gestiones de permisos y licencias a nivel municipal, estatal o federal; procedimiento contencioso administrativo; interposición de recursos y litigio de resoluciones administrativas. Entre ellas, impugnación de licitaciones y negativas a permisos y licencias.</p>
+          </div>
+          <div class="col-md-3">
+           <figure class="imagens2">
 
-<div class="area">
-	<div class="areauno pull-left">
-		ÁREA <br>
-		AUDITORÍA
-	</div>
-	<div class="areados pull-right">
-		CALCUEXPRESS REPARADORES DE EMPRESAS practica exámenes sistemáticos a los estados financieros, registros y transacciones de cada uno de sus clientes para determinar la adherencia a: los principios de contabilidad, generalmente aceptados, a las políticas del ente auditado y a los preceptos legales locales. <br><br>
+            </figure>
+            <h3 class="text-center">DERECHO FISCAL</h3>
+            <p class="">Asesoría sobre recursos, medios de defensa y en general litigio fiscal respecto a asuntos del orden Federal, Estatal o Municipal.</p>
+          </div>
+          <div class="col-md-3">
+            <figure class="imagens3">
 
-		De la misma manera, mediante el examen exhaustivo de la información contable se puede detectar si los estados financieros están adecuadamente presentados en las fechas y periodos indicados.
-	</div>
-</div>
-<div class="area2">
-	<div class="areauno pull-left">
-		ÁREA <br>
-		CONTABLE
-	</div>
-	<div class="areados pull-right">
-		Ofrecemos apoyo en el procesamiento y actualización de la información interna y externa de su empresa, con el fin de tomar decisiones con base a informes que reflejan la situación financiera de su organización.<br><br>
-		Mediante el examen minucioso de las aplicaciones técnicas relacionadas con la normatización contable, se direccionará a la gerencia para tomar acciones encaminadas a perfeccionar el sistema contable que esté operando, dentro de un proceso de control y supervisión contable.
-	</div>
-</div>
+            </figure>
+            <h3 class="text-center">DERECHO LABORAL</h3>
+            <p class="r">Asesoría respecto a asuntos de derecho laboral de naturaleza individual o colectiva; entre otros: Formulación y revisión de contratos de trabajo, revisión e implementación de los diversos mecanismos de control, trámites de sustitución patronal, transferencias de personal. </p>
+          </div>
+          <div class="col-md-3">
+            <figure class="imagens4">
 
-<div class="area">
-	<div class="areauno pull-left">
-		ÁREA <br>
-		TRIBUTARIA
-	</div>
-	<div class="areados pull-right">
-		Comprende la aplicación de estrategias y procedimientos de carácter impositivo que permitan medir el beneficio obtenido por la empresa, derivado de la interpretación de las normas tributarias. <br><br>
+            </figure>
+            <h3 class="text-center">DERECHO PENAL</h3>
+            <p class="">Tratamiento de cuestiones penales de diversa índole.</p>
+          </div>
+        </div>
 
-		Se suministra la información profesional y pertinente sobre las aplicaciones prácticas del régimen fiscal vigente, buscando la mejor alternativa que genere beneficios económicos al cliente.
-	</div>
-</div>
-<div class="area4">
-	<div class="areauno pull-left">
-		ÁREA <br>
-		FISCAL
-	</div>
-	<div class="areados pull-right">
-		El servicio de revisoría fiscal busca que las empresas cuenten con un garante en el marco fiscal, contable y tributario acorde con las normas vigentes.<br><br>
+        <br><br>
+        <div class="row">
+          <div class="col-md-3">
+            <img src="../img/da.jpg"
+            class="img-responsive">
+            <h3 class="text-center">DERECHO CIVIL Y FAMILIAR</h3>
+            <p class="">Asesoría respecto a asuntos de derecho civil en general; formulación de contratos y convenios de todo tipo, asesoría sobre asuntos relacionados con derechos reales (cuestiones de arrendamiento, constitución de garantías, inmobiliario, entre otros); sucesiones.</p>
+          </div>
+          <div class="col-md-3">
+            <img src="../img/da.jpg"
+            class="img-responsive">
+            <h3 class="text-center">DERECHO MERCANTIL Y CORREDURÍA PÚBLICA</h3>
+            <p class="">Respecto a cualquier requerimiento que pudiera presentarse a cualquier inversionista, empresa o comerciante, abarcando desde la constitución de nuevas empresas, todas las necesidades que llegaren a presentarse durante su operación, hasta su liquidación.</p>
+          </div>
+          <div class="col-md-3">
+            <img src="../img/da.jpg"
+            class="img-responsive img-rounded">
+            <h3 class="text-center">AUDITORIAS LEGALES</h3>
+            <p class="">Auditoría sobre el estado de sus más importantes documentos corporativos, contratos, mecanismos de control y litigios.</p>
+          </div>
+          <div class="col-md-3">
+            <img src="../img/da.jpg"
+            class="img-responsive">
+            <h3 class="text-center">MIGRACIÓN</h3>
+            <p class="">Asesoría y gestión para adquirir la documentación que permita a los extranjeros trabajar y permanecer legalmente en el país.</p>
+          </div>
+        </div>
+      </div>
+    </div>
 
-		Para ello, CALCUEXPRESS REPARADORES DE EMPRESAS S.C.P está en permanente actualización profesional, con el fin de lograr una aplicación conceptual, innovadora e integral en el ejercicio de la revisoría fiscal en cada compañía.
-	</div>
-</div>
+
+
 <?php include 'pie.php' ?>
 <script src="../js/jquery.js"></script>
 <script src="../js/bootstrap.js"></script>
@@ -82,6 +99,3 @@
 </body>
 </html>
 
-<!-- <div class="contenedor">
-		<img src="../img/serviciosc.jpg" alt="" class="imagen img-responsive" >
-	</div> -->
