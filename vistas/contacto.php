501c3435ab763d78a4406267769d74dac3d61258
esteban1234 <tva_libra@hotmail.com>
2018-02-14 18:13:20 -0600

    pagina lista

--- vistas/contacto.php
+++ vistas/contacto.php
@@ -1,75 +1,91 @@
 <!DOCTYPE html>
 <?php include 'encabezado2.php' ?>
 <body>
-	<div class="bannermenu2">
-	<div class="contenlogomenu">
-		<figure class="logo">
-			<img src="../img/calcu.png" alt="" class="img-responsive pull-left">
-		</figure>
-		<div id="header__icon" class="menu-opcion">
-		  <i id="open" class="fa fa-align-justify fa-2x" aria-hidden="true"></i>
-		  <i id="close" class="fa fa-close fa-2x close"  aria-hidden="true"></i>
-		  <p>Menu</p>
-		</div>
-		<nav class=" menu pull-right">
-			<ul>
-				<li><a href="../index.php">INDEX</a></li>
-				<li><a href="nosotros.php">NOSOTROS</a></li>
-				<li><a href="servicios.php">SERVICIOS</a></li>
-				<li><a class="select" href="contacto.php">CONTACTO</a></li>
-			</ul>
-		</nav>
-	</div>
+	
+
+  <header>
+    <figure class="logo pull-left">
+        <img src="../img/dafo.png" alt="DAFO" class="img-responsive">
+    </figure>
+
+    <nav class="pull-right">
+        <ul>
+            <a class="select" href="../index.php">INICIO</a>
+            <a class="select" href="nosotros.php">NOSOTROS</a>
+            <a class="select" href="servicios.php">SERVICIOS</a>
+            <a class="select" id="selector" href="contacto.php">CONTACTO</a>
+        </ul>
+    </nav>
+</header>
+
+<div class="banner4">
+   <div class="fondo4">
+        <p class="uno">EXPERTOS EN <br>  DERECHO CIVIL Y DERECHO LABORAL</p>
+   </div>
 </div>
 
-<article class="empresa">
-	<p>CONTACTO</p>
-</article>
+<p class="textcontac"> Si desea más información sobre nuestros servicios, puede contactarnos a través de nuestros teléfonos, correo electrónico o en nuestras oficinas. </p>
 
-<div class="mapa">
-	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.6751614859095!2d-99.17229638564491!3d19.426436386887875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff4a2e34317d%3A0x7d6e7dc8caf395e7!2sPaseo+de+la+Reforma+383%2C+Cuauht%C3%A9moc%2C+06500+Ciudad+de+M%C3%A9xico%2C+CDMX!5e0!3m2!1ses-419!2smx!4v1497036187970" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-</div>
+<section id="contact" class="content-section text-center">
+        <div class="contact-section">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                  <form class="form-horizontal">
+                    <div class="form-group">
+                      <label for="exampleInputName2">Nombre Completo</label>
+                      <input type="text" class="form-control" id="exampleInputName2" placeholder="">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputName2">Teléfono</label>
+                      <input type="text" class="form-control" id="exampleInputName2" placeholder="">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputEmail2">Correo</label>
+                      <input type="email" class="form-control" id="exampleInputEmail2" placeholder="">
+                    </div>
+                    <div class="form-group ">
+                      <label for="exampleInputText">Comentario</label>
+                     <textarea  class="form-control" placeholder=""></textarea> 
+                    </div>
+                    <button type="submit" class="btn btn-default">Enviar Mensaje</button>
+                  </form>
 
-<div class="bg-faded py-5 form1">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <h3 class="text-tit-contacto">CALCUEXPRESS REPARADORES DE EMPRESAS S.C.P</h3>
-          <p class="text-info-contacto">
-            <br>Av. Paseo de la Reforma #383 <br>
-                por C. Rio Nilo y Rio Guadalquivir <br>
-                Col. Cuauhtemoc, Cd. de México C.P. 06500
-            <br> <abbr title="Phone">Teléfono:</abbr>PENDIENTE <br>
-            <abbr title="Mail">Correo:</abbr>info@calcuexpress.com <br>
-            <abbr title="Mail">Horario:</abbr>Lunes a Viernes de 9am a 6pm
-            </p>
+                  <hr>
+                    <h3>DATOS DE OFICINA</h3>
+
+     
+                </div>
+              </div>
+            </div>
         </div>
-        <div class="col-md-6">
-          <h2 class="pi-item">Formulario de contacto</h2>
-          <form onSubmit="return false">
-            <fieldset class="form-group"> <label for="exampleInputEmail1">Nombre completo</label>
-              <input type="text" id="txtNOMBRE" name="txtNOMBRE" class="form-control" id="exampleInputEmail1" placeholder="Escribe tu nombre completo"> </fieldset>
-
-              <fieldset class="form-group"> <label for="exampleInputEmail1">Teléfono</label>
-              <input type="text" id="txtTELEFONO" name="txtTELEFONO" class="form-control" id="exampleInputEmail1" placeholder="Escribe tu teléfono"> </fieldset>
-
-            <fieldset class="form-group"> <label for="exampleInputEmail1">Correo</label>
-              <input type="email" id="txtCORREO" name="txtCORREO" class="form-control" id="exampleInputEmail1" placeholder="Escribe tu correo"> </fieldset>
-
-              <fieldset class="form-group"> <label for="exampleInputEmail1">Comentario</label>
-               <textarea type="text" id="txtCOMENTARIO" name="txtCOMENTARIO" class="form-control" rows="3" placeholder="Escribe tu comentario"></textarea>
-              </fieldset>
-
-							<div id="_AJAX_PRE_"></div>
-							</div>
-
-            <button type="submit" class="btn btn-warning pull-right" id="btnContactUs" onclick="sendCORREO()">Enviar</button>
-						<!-- <button type="submit" class="btn btn-warning pull-right" id="btnContactUs"  onclick="sendCORREO()">ENVIAR</button> -->
-          </form>
+              <br><br>
+         <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <i class="fas fa-map-marker-alt fa-7x"></i>
+            <h2 class="text-center">DIRECCION</h2>
+            <p class="text-center">PENDIENTE</p>
+          </div>
+          <div class="col-md-3">
+            <i class="fas fa-phone fa-7x"></i>
+            <h2 class="text-center">TELEFONO</h2>
+            <p class="text-center">PENDIENTE</p>
+          </div>
+          <div class="col-md-3">
+            <i class="far fa-envelope fa-7x"></i>
+            <h2 class="text-center">CORREO</h2>
+            <p class="text-center">info@administradoradafo.com</p>
+          </div>
+          <div class="col-md-3">
+            <i class="far fa-clock fa-7x"></i>
+            <h2 class="text-center">HORARIO</h2>
+            <p class="text-center">Lunes a Viernes de 9 am a 6 pm</p>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
+
+      </section>
 
 <?php include 'pie.php' ?>
 <script src="../js/jquery.js"></script>
--- vistas/servicios.php
+++ vistas/servicios.php
@@ -30,62 +30,68 @@
       	<h1>ÁREAS DE PRACTRICA</h1><br>
       	<p>Nuestra empresa ofrece servicios de Litigio, Consultoría legal y Correduría Pública en las siguientes áreas del derecho:</p> <br><br>
         <div class="row">
-          <div class="col-md-3">
-            <figure class="imagens">
-
-            </figure>
-            <h3 class="text-center">DERECHO ADMINISTRATIVO</h3>
-            <p class="">Gestiones de permisos y licencias a nivel municipal, estatal o federal; procedimiento contencioso administrativo; interposición de recursos y litigio de resoluciones administrativas. Entre ellas, impugnación de licitaciones y negativas a permisos y licencias.</p>
-          </div>
-          <div class="col-md-3">
+          
+          <div class="col-md-3" style="border: 1px #ccc solid; height: 435px;">
            <figure class="imagens2">
 
             </figure>
             <h3 class="text-center">DERECHO FISCAL</h3>
             <p class="">Asesoría sobre recursos, medios de defensa y en general litigio fiscal respecto a asuntos del orden Federal, Estatal o Municipal.</p>
           </div>
-          <div class="col-md-3">
+          <div class="col-md-3" style="border: 1px #ccc solid; height: 435px;">
             <figure class="imagens3">
 
             </figure>
             <h3 class="text-center">DERECHO LABORAL</h3>
             <p class="r">Asesoría respecto a asuntos de derecho laboral de naturaleza individual o colectiva; entre otros: Formulación y revisión de contratos de trabajo, revisión e implementación de los diversos mecanismos de control, trámites de sustitución patronal, transferencias de personal. </p>
           </div>
-          <div class="col-md-3">
+          <div class="col-md-3" style="border: 1px #ccc solid; height: 435px;">
             <figure class="imagens4">
 
             </figure>
             <h3 class="text-center">DERECHO PENAL</h3>
             <p class="">Tratamiento de cuestiones penales de diversa índole.</p>
           </div>
+          <div class="col-md-3" style="border: 1px #ccc solid; height: 435px;">
+            <figure class="imagens8">
+
+            </figure>
+            <h3 class="text-center">MIGRACIÓN</h3>
+            <p class="">Asesoría y gestión para adquirir la documentación que permita a los extranjeros trabajar y permanecer legalmente en el país.</p>
+          </div>
         </div>
 
         <br><br>
         <div class="row">
-          <div class="col-md-3">
-            <img src="../img/da.jpg"
-            class="img-responsive">
+          <div class="col-md-3" style="border: 1px #ccc solid; height: 435px;">
+           <figure class="imagens5">
+
+            </figure>
             <h3 class="text-center">DERECHO CIVIL Y FAMILIAR</h3>
             <p class="">Asesoría respecto a asuntos de derecho civil en general; formulación de contratos y convenios de todo tipo, asesoría sobre asuntos relacionados con derechos reales (cuestiones de arrendamiento, constitución de garantías, inmobiliario, entre otros); sucesiones.</p>
           </div>
-          <div class="col-md-3">
-            <img src="../img/da.jpg"
-            class="img-responsive">
-            <h3 class="text-center">DERECHO MERCANTIL Y CORREDURÍA PÚBLICA</h3>
-            <p class="">Respecto a cualquier requerimiento que pudiera presentarse a cualquier inversionista, empresa o comerciante, abarcando desde la constitución de nuevas empresas, todas las necesidades que llegaren a presentarse durante su operación, hasta su liquidación.</p>
+          <div class="col-md-3" style="border: 1px #ccc solid; height: 435px;">
+            <figure class="imagens6">
+
+            </figure>
+            <h3 class="text-center">DERECHO MERCANTIL</h3>
+            <p class="">Cualquier requerimiento que pudiera presentarse a cualquier inversionista, empresa o comerciante, abarcando desde la constitución de nuevas empresas, todas las necesidades que llegaren a presentarse durante su operación, hasta su liquidación.</p>
           </div>
-          <div class="col-md-3">
-            <img src="../img/da.jpg"
-            class="img-responsive img-rounded">
+          <div class="col-md-3" style="border: 1px #ccc solid; height: 435px;">
+            <figure class="imagens7">
+
+            </figure>
             <h3 class="text-center">AUDITORIAS LEGALES</h3>
             <p class="">Auditoría sobre el estado de sus más importantes documentos corporativos, contratos, mecanismos de control y litigios.</p>
           </div>
-          <div class="col-md-3">
-            <img src="../img/da.jpg"
-            class="img-responsive">
-            <h3 class="text-center">MIGRACIÓN</h3>
-            <p class="">Asesoría y gestión para adquirir la documentación que permita a los extranjeros trabajar y permanecer legalmente en el país.</p>
+            <div class="col-md-3" style="border: 1px #ccc solid; height: 435px;">
+            <figure class="imagens">
+
+            </figure>
+            <h3 class="text-center">DERECHO ADMINISTRATIVO</h3>
+            <p class="">Gestiones de permisos y licencias a nivel municipal, estatal o federal; procedimiento contencioso administrativo; interposición de recursos y litigio de resoluciones administrativas. Entre ellas, impugnación de licitaciones y negativas a permisos y licencias.</p>
           </div>
+
         </div>
       </div>
     </div>
