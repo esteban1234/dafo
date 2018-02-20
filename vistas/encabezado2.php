b92be26f1336968eec2d05be3e4effe63451a4ea
esteban1234 <tva_libra@hotmail.com>
2018-02-20 11:12:02 -0600

    metas

--- vistas/encabezado2.php
+++ vistas/encabezado2.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-
+	<meta name="google-site-verification" content="zW3eVjgC18R6Il1EbN-uD7uM8ahaslJYcYxyncnl0uA" />
 	<link rel="icon" type="image/png" href="../img/favicon.png" />
 	<title>ADMINISTRADORA Y CONSULTORA DAFO</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
--- vistas/envia.php
+++ vistas/envia.php
@@ -22,8 +22,8 @@ $mail->From = $correo;
 $mail->FromName = $nombre;
 
 //To address and name
-// $mail->addAddress("info@administradoradafo.com");
-$mail->addAddress("ballina.santiago@gmail.com");
+$mail->addAddress("info@administradoradafo.com");
+
 
 
 $mail->isHTML(true);
